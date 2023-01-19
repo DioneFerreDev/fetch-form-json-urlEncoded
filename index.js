a12b64ec2d48184ea39671e3391fde205b3fcc5d
Dione Ferreira Teixeira <jhonimirassol@gmail.com>
2023-01-18 21:37:03 -0300

    final-version

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const bcrypt = require("bcrypt");
 
 
 
-const salt = bcrypt.genSaltSync(14);
+// const salt = bcrypt.genSaltSync(14);
 
-// lembrar de gerar um id para inserir em cada carta do deck via class item
-const loki = require("lokijs");
-const db = new loki(path.join(__dirname, '/data/db.json'));
-var cards = db.addCollection('users');
-cards.insert({ name:"My User",password:bcrypt.hashSync("user999",salt) });
-db.save(err => {
-  if (err) console.log(err);
-});
+// // lembrar de gerar um id para inserir em cada carta do deck via class item
+// const loki = require("lokijs");
+// const db = new loki(path.join(__dirname, '/data/db.json'));
+// var cards = db.addCollection('users');
+// cards.insert({ name:"My User",password:bcrypt.hashSync("user999",salt) });
+// db.save(err => {
+//   if (err) console.log(err);
+// });
 
-db.saveDatabase()
+// db.saveDatabase()
 
 
 
